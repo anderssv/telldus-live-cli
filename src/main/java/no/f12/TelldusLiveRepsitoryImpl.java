33675ea07c911e411ceaedd7f7e4802c88b0d1b9
Anders Sveen <anders@f12.no>
2016-01-28 20:30:31 +0100

    Just some formatting

--- src/main/java/no/f12/TelldusLiveRepsitoryImpl.java
+++ src/main/java/no/f12/TelldusLiveRepsitoryImpl.java
@@ -20,45 +20,41 @@ public class TelldusLiveRepsitoryImpl implements TelldusRepository {
 
 	@Override
 	public void turnDeviceOn(String deviceId) {
-		new TelldusCommandTemplate(this).execute("device/turnOn", deviceId,
-				new CommandCallback<String>() {
-					@Override
-					public String doCommand(JsonNavigator jsonMap) {
-						this.verifySuccess(jsonMap, deviceId, "turn on");
-						return "";
-					}
-				});
+		new TelldusCommandTemplate(this).execute("device/turnOn", deviceId, new CommandCallback<String>() {
+			@Override
+			public String doCommand(JsonNavigator jsonMap) {
+				this.verifySuccess(jsonMap, deviceId, "turn on");
+				return "";
+			}
+		});
 	}
 
 	@Override
 	public void turnDeviceOff(String deviceId) {
-		new TelldusCommandTemplate(this).execute("device/turnOff", deviceId,
-				new CommandCallback<String>() {
-					@Override
-					public String doCommand(JsonNavigator jsonMap) {
-						this.verifySuccess(jsonMap, deviceId, "turn off");
-						return "";
-					}
-				});
+		new TelldusCommandTemplate(this).execute("device/turnOff", deviceId, new CommandCallback<String>() {
+			@Override
+			public String doCommand(JsonNavigator jsonMap) {
+				this.verifySuccess(jsonMap, deviceId, "turn off");
+				return "";
+			}
+		});
 	}
 
 	@Override
 	public List<Device> getDevices() {
-		return new TelldusCommandTemplate(this).query("devices/list",
-				new CommandCallback<List<Device>>() {
-					@Override
-					public List<Device> doCommand(JsonNavigator jsonMap) {
-						List<String> deviceIds = jsonMap
-								.findAll("$.device[*].id");
+		return new TelldusCommandTemplate(this).query("devices/list", new CommandCallback<List<Device>>() {
+			@Override
+			public List<Device> doCommand(JsonNavigator jsonMap) {
+				List<String> deviceIds = jsonMap.findAll("$.device[*].id");
 
-						List<Device> devices = new ArrayList<>();
-						for (String devideId : deviceIds) {
-							devices.add(new Device(Integer.parseInt(devideId)));
-						}
+				List<Device> devices = new ArrayList<>();
+				for (String devideId : deviceIds) {
+					devices.add(new Device(Integer.parseInt(devideId)));
+				}
 
-						return devices;
-					}
-				});
+				return devices;
+			}
+		});
 	}
 
 	@Override
@@ -67,24 +63,21 @@ public class TelldusLiveRepsitoryImpl implements TelldusRepository {
 		params.put("id", id);
 		params.put("supportedMethods", "1");
 
-		return new TelldusCommandTemplate(this).execute("device/info", params,
-				new CommandCallback<Boolean>() {
-					@Override
-					public Boolean doCommand(JsonNavigator jsonMap) {
-						String deviceState = (String) jsonMap.get("$.state",
-								String.class);
-						Boolean result = Boolean.FALSE;
-						if (deviceState.equals("1")) {
-							result = Boolean.TRUE;
-						}
+		return new TelldusCommandTemplate(this).execute("device/info", params, new CommandCallback<Boolean>() {
+			@Override
+			public Boolean doCommand(JsonNavigator jsonMap) {
+				String deviceState = (String) jsonMap.get("$.state", String.class);
+				Boolean result = Boolean.FALSE;
+				if (deviceState.equals("1")) {
+					result = Boolean.TRUE;
+				}
 
-						return result;
-					}
-				});
+				return result;
+			}
+		});
 	}
 
-	public OAuthRequest createAndSignRequest(String url,
-			Map<String, String> parameters) {
+	public OAuthRequest createAndSignRequest(String url, Map<String, String> parameters) {
 
 		OAuthRequest request = createRequest(url, parameters);
 
@@ -110,13 +103,12 @@ public class TelldusLiveRepsitoryImpl implements TelldusRepository {
 		String publicKey = props.getProperty("telldus.api.key.public");
 		String secretKey = props.getProperty("telldus.api.key.secret");
 
-		OAuthService oService = new ServiceBuilder().provider(GoogleApi.class)
-				.apiKey(publicKey).apiSecret(secretKey).build();
+		OAuthService oService = new ServiceBuilder().provider(GoogleApi.class).apiKey(publicKey).apiSecret(secretKey)
+				.build();
 		return oService;
 	}
 
-	private OAuthRequest createRequest(String url,
-			Map<String, String> parameters) {
+	private OAuthRequest createRequest(String url, Map<String, String> parameters) {
 		// Create, sign and send request.
 		OAuthRequest request = new OAuthRequest(Verb.GET, telldusUrl(url));
 
@@ -140,39 +132,34 @@ public class TelldusLiveRepsitoryImpl implements TelldusRepository {
 		Map<String, String> params = new HashMap<>();
 		params.put("includeValues", "1");
 
-		Map<String, String> sensorValues = new TelldusCommandTemplate(this)
-				.execute("sensors/list", params,
-						new CommandCallback<Map<String, String>>() {
-							@Override
-							public Map<String, String> doCommand(
-									JsonNavigator jsonMap) {
-								List<Map<String, String>> sensors = jsonMap
-										.findAll("$.sensor");
+		Map<String, String> sensorValues = new TelldusCommandTemplate(this).execute("sensors/list", params,
+				new CommandCallback<Map<String, String>>() {
+					@Override
+					public Map<String, String> doCommand(JsonNavigator jsonMap) {
+						List<Map<String, String>> sensors = jsonMap.findAll("$.sensor");
 
-								Map<String, String> temperatureValues = new HashMap<String, String>();
-								for (Map<String, String> sensor : sensors) {
-									temperatureValues.put(sensor.get("id"),
-											sensor.get("temp"));
-								}
+						Map<String, String> temperatureValues = new HashMap<String, String>();
+						for (Map<String, String> sensor : sensors) {
+							temperatureValues.put(sensor.get("id"), sensor.get("temp"));
+						}
 
-								return temperatureValues;
-							}
-						});
+						return temperatureValues;
+					}
+				});
 
 		return sensorValues.get(deviceId);
 	}
 
 	@Override
 	public List<DeviceEvent> getDeviceHistory(String deviceId) {
-		Map<String, String> params = new HashMap<String, String>();
+		Map<String, String> params = new HashMap<>();
 		params.put("to", "1000000000000");
-		return new TelldusCommandTemplate(this).execute("device/history",
-				deviceId, params, new CommandCallback<List<DeviceEvent>>() {
+		return new TelldusCommandTemplate(this).execute("device/history", deviceId, params,
+				new CommandCallback<List<DeviceEvent>>() {
 
 					@Override
 					List<DeviceEvent> doCommand(JsonNavigator result) {
-						List<Map<String, Integer>> rawEvents = result
-								.findAll("$.history");
+						List<Map<String, Integer>> rawEvents = result.findAll("$.history");
 						List<DeviceEvent> deviceEvents = new ArrayList<DeviceEvent>();
 
 						for (Map<String, Integer> event : rawEvents) {
